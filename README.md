1b13150388c283d4ba9de174afb8b385789cfe7f
Ümit Akköse <umieat@gmail.com>
2021-09-15 16:23:32 +0300

    Adjust nucleotide profile during simulation Reads from Input Sequencing can be used instead of reference genome

--- README.md
+++ README.md
@@ -5,23 +5,25 @@ Tool for generating synthetic next-generation sequencing reads with same nucleot
 ## Usage
 
 ```
-boquila 0.4.1
+boquila 0.5.0
 Generate NGS reads with same nucleotide distribution as input file
 Generated reads will be written to stdout
 By default input and output format is FASTQ
 
 USAGE:
-    boquila [FLAGS] [OPTIONS] <src> --ref <FILE> --regions <FILE>
+    boquila [FLAGS] [OPTIONS] <src>
 
 ARGS:
     <src>    Model file
 
 FLAGS:
-        --fasta      Change input and output format to FASTA
-    -h, --help       Prints help information
-    -V, --version    Prints version information
+        --fasta         Change input and output format to FASTA
+        --inseqFasta    Change the input sequencing format to FASTA
+    -h, --help          Prints help information
+    -V, --version       Prints version information
 
 OPTIONS:
+        --inseq <FILE>      Input sequencing reads to be used instead of reference genome
         --kmer <INT>        Kmer size to be used while calculating frequency [default: 1]
         --bed <FILE>        File name in which the simulated reads will be saved in BED format
         --ref <FILE>        Reference FASTA
@@ -36,6 +38,40 @@ Generated reads will be written to stdout in FASTA or FASTQ format.
 If `--bed` option is provided, generated reads also will be written to given file in `BED6` format.
 
 Sample `regions` file for Homo sapiens (human) genome assembly GRCh38 (hg38) is provided as `GRCh38.ron`
+
+If Input Sequencing reads will be used for simulation, they should be provided with `--inseq` argument, instead of using `--ref` and `--regions`.
+
+### Examples
+
+Simple usage
+```
+boquila input_reads.fq --ref ref_genome.fa --regions GRCh38.ron > out.fq
+```
+
+Using seed for RNG
+```
+boquila input_reads.fq --ref ref_genome.fa --regions GRCh38.ron --seed 7 > out.fq
+```
+
+Using reads that are in FASTA format
+```
+boquila input_reads.fasta --fasta --ref ref_genome.fa --regions GRCh38.ron > out.fa
+```
+
+Saving output in BED format
+```
+boquila input_reads.fq --ref ref_genome.fa --regions GRCh38.ron --bed out.bed > out.fq
+```
+
+Using Input Sequencing instead of reference genome
+```
+boquila input_reads.fq --inseq ref_genome.fa --regions GRCh38.ron > out.fq
+```
+
+Using Input Sequencing reads which are in FASTA format
+```
+boquila input_reads.fq --inseqFasta --inseq ref_genome.fa --regions GRCh38.ron > out.fq
+```
 
 ## Installation
 
@@ -54,5 +90,5 @@ $ git clone https://github.com/CompGenomeLab/boquila.git
 $ cd boquila
 $ cargo build --release
 $ ./target/release/boquila --version
-0.4.1
+0.5.0
 ```
--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use anyhow::Context;
 use clap::{App, Arg};
 use itertools::Itertools;
 use nanoserde::DeRon;
-use rand::{rngs::SmallRng, Rng, SeedableRng};
+use rand::{prelude::SliceRandom, rngs::SmallRng, Rng, SeedableRng};
 use rand_distr::{Distribution, WeightedAliasIndex};
 use seq_io::{
     fasta::{write_to as write_to_fa, Reader as faReader, Record as faRecord},
@@ -26,6 +26,7 @@ struct Region {
 trait Kmer {
     fn from_k_with_key(kmer: usize, key: String) -> Self;
     fn normalize(&mut self);
+    fn normalize_immutable(&self) -> HashMap<String, f32>;
 }
 
 impl Kmer for HashMap<String, f32> {
@@ -64,6 +65,16 @@ impl Kmer for HashMap<String, f32> {
         self.values_mut().for_each(|v| {
             *v /= sum;
         });
+    }
+
+    fn normalize_immutable(&self) -> HashMap<String, f32> {
+        // let sum: f32 = self.values().sum();
+        let mut map: HashMap<String, f32> = HashMap::new();
+        self.iter().for_each(|(k, v)| {
+            map.insert(k.to_string(), *v);
+        });
+        map.normalize();
+        map
     }
 }
 
@@ -146,6 +157,34 @@ fn normalize_distmap(dist_map: &mut HashMap<usize, Vec<HashMap<String, f32>>>) {
     })
 }
 
+// normalize distmap with another distmap
+fn adjust_distmap(
+    real_map: &HashMap<usize, Vec<HashMap<String, f32>>>,
+    dist_map: &mut HashMap<usize, Vec<HashMap<String, f32>>>,
+    second_map: &HashMap<usize, Vec<HashMap<String, f32>>>,
+) {
+    let mut normalized_second_map: HashMap<usize, Vec<HashMap<String, f32>>> = HashMap::new();
+    second_map.iter().for_each(|(k, v)| {
+        normalized_second_map.insert(*k, v.iter().map(|el| el.normalize_immutable()).collect());
+    });
+    dist_map.iter_mut().for_each(|(key, value)| {
+        if let Some(dist) = normalized_second_map.get(key) {
+            value.iter_mut().enumerate().for_each(|(inner_index, n)| {
+                n.iter_mut().for_each(|(inner_key, inner_value)| {
+                    *inner_value += real_map
+                        .get(key)
+                        .unwrap()
+                        .get(inner_index)
+                        .unwrap()
+                        .get(inner_key)
+                        .unwrap()
+                        - dist.get(inner_index).unwrap().get(inner_key).unwrap();
+                })
+            })
+        }
+    })
+}
+
 enum Fastx<'a> {
     Fasta(&'a str),
     Fastq(&'a str),
@@ -215,6 +254,13 @@ impl<'a> Fastx<'a> {
     ) -> anyhow::Result<()> {
         let genome = parse_genome(genome_path)?;
         let (records, ndist) = self.parse(kmer)?;
+
+        let mut dyn_dist = ndist.clone();
+        let mut out_dist: HashMap<usize, Vec<HashMap<String, f32>>> = HashMap::new();
+
+        let mut counter = 0;
+        let checkpoint = records.len() / 10;
+
         let regions_str = read_to_string(regions_path)?;
         let regions: Vec<Region> =
             DeRon::deserialize_ron(&regions_str).context("Failed to parse regions file")?;
@@ -242,7 +288,7 @@ impl<'a> Fastx<'a> {
         let rng_dist = WeightedAliasIndex::new(weights).unwrap();
 
         for (index, record) in records.into_iter().enumerate() {
-            if let Some(dist) = ndist.get(&record.len) {
+            if let Some(dist) = dyn_dist.get(&record.len) {
                 let mut n_reads: Vec<(&[u8], Region)> = Vec::new();
                 let mut n_reads_rev: Vec<(Vec<u8>, Region)> = Vec::new();
                 let d: Vec<(Vec<u8>, Region)>;
@@ -308,6 +354,8 @@ impl<'a> Fastx<'a> {
                     _ => "+",
                 };
 
+                update_distmap(&mut out_dist, &seq, kmer);
+
                 if let Some(ref mut bed_file) = bed_file {
                     bed_file.write_all(
                         format!(
@@ -335,10 +383,128 @@ impl<'a> Fastx<'a> {
                     }
                 }
             };
+            counter += 1;
+            if counter == checkpoint {
+                // adjust the dyn_dist
+                counter = 0;
+                adjust_distmap(&ndist, &mut dyn_dist, &out_dist);
+            }
         }
 
         Ok(())
     }
+
+    fn sim_input_seq(
+        &self,
+        input_seq_path: &str,
+        seed: Option<&str>,
+        kmer: usize,
+        fasta: bool,
+    ) -> anyhow::Result<()> {
+        let input_records = parse_input_seq(input_seq_path, fasta)?;
+        let (records, ndist) = self.parse(kmer)?;
+
+        let mut dyn_dist = ndist.clone();
+        let mut out_dist: HashMap<usize, Vec<HashMap<String, f32>>> = HashMap::new();
+
+        let mut counter = 0;
+        let checkpoint = records.len() / 10;
+
+        let mut rng: SmallRng = match seed {
+            Some(seed) => SeedableRng::seed_from_u64(seed.parse()?),
+            None => SeedableRng::from_entropy(),
+        };
+
+        let stdout = stdout();
+
+        for record in records {
+            if let Some(dist) = dyn_dist.get(&record.len) {
+                let mut n_reads: Vec<(&[u8], Region)> = Vec::new();
+                for _ in 0..10 {
+                    let choosen = input_records
+                        .choose(&mut rng)
+                        .context("Slice should not be empty")?;
+                    let start_pos = rng.gen_range(0usize..choosen.len() as usize - record.len);
+                    let end_pos = start_pos + record.len;
+                    let seq = choosen
+                        .get(start_pos..end_pos)
+                        .context("Sequence is shorter than reads")?;
+                    n_reads.push((
+                        seq.as_bytes(),
+                        Region {
+                            name: "hi".to_string(),
+                            start: start_pos as u64,
+                            end: end_pos as u64,
+                        },
+                    ));
+                }
+
+                let (seq, _) = dist.top_seq(&n_reads, kmer);
+
+                update_distmap(&mut out_dist, seq, kmer);
+
+                match *self {
+                    Fastx::Fastq(_) => {
+                        write_to(
+                            &stdout,
+                            record.head.as_bytes(),
+                            seq,
+                            record.qual.unwrap().as_bytes(),
+                        )?;
+                    }
+                    Fastx::Fasta(_) => {
+                        write_to_fa(&stdout, record.head.as_bytes(), seq)?;
+                    }
+                }
+            };
+            counter += 1;
+            if counter == checkpoint {
+                // adjust the dyn_dist
+                counter = 0;
+                adjust_distmap(&ndist, &mut dyn_dist, &out_dist);
+            }
+        }
+
+        Ok(())
+    }
+}
+
+fn parse_input_seq(filename: &str, fasta: bool) -> anyhow::Result<Vec<String>> {
+    let mut records: Vec<String> = Vec::new();
+
+    if fasta {
+        let mut reader = faReader::from_path(filename)
+            .with_context(|| format!("Failed to open file {}", filename))?;
+
+        while let Some(record) = reader.next() {
+            let record = record.context("Invalid record")?;
+            let record_seq = record.seq();
+            if !record_seq.is_empty() {
+                records.push(
+                    str::from_utf8(record_seq)
+                        .context("Input sequencing read is not in utf8 format")?
+                        .to_string(),
+                );
+            }
+        }
+    } else {
+        let mut reader = fqReader::from_path(filename)
+            .with_context(|| format!("Failed to open file {}", filename))?;
+
+        while let Some(record) = reader.next() {
+            let record = record.context("Invalid record")?;
+            let record_seq = record.seq();
+            if !record_seq.is_empty() {
+                records.push(
+                    str::from_utf8(record_seq)
+                        .context("Input sequencing read is not in utf8 format")?
+                        .to_string(),
+                );
+            }
+        }
+    }
+
+    Ok(records)
 }
 
 fn parse_genome(filename: &str) -> anyhow::Result<HashMap<String, Vec<u8>>> {
@@ -377,7 +543,7 @@ fn reverse_complement(input: &[u8]) -> Vec<u8> {
 
 fn main() -> anyhow::Result<()> {
     let matches = App::new("boquila")
-        .version("0.4.1")
+        .version("0.5.0")
         .about("Generate NGS reads with same nucleotide distribution as input file\nGenerated reads will be written to stdout\nBy default input and output format is FASTQ")
         .arg(Arg::new("src").about("Model file").index(1).required(true))
         .arg(
@@ -386,14 +552,12 @@ fn main() -> anyhow::Result<()> {
                 .long("ref")
                 .takes_value(true)
                 .value_name("FILE")
-                .required(true),
-        )
-        .arg(
+                .required_unless_present("inputseq"),
+        ).arg(
             Arg::new("fasta")
                 .about("Change input and output format to FASTA")
                 .long("fasta")
-        )
-        .arg(
+        ).arg(
             Arg::new("regions")
                 .about(
                     "RON formatted file containing genomic regions that generated reads will be selected from",
@@ -401,7 +565,20 @@ fn main() -> anyhow::Result<()> {
                 .long("regions")
                 .takes_value(true)
                 .value_name("FILE")
-                .required(true),
+                .required_unless_present("inputseq"),
+        ).arg(
+            Arg::new("inputseq")
+                .about(
+                    "Input sequencing reads to be used instead of reference genome",
+                )
+                .long("inseq")
+                .takes_value(true)
+                .value_name("FILE")
+                .required_unless_present_all(&["ref", "regions"]),
+        ).arg(
+            Arg::new("inputseqfasta")
+                .about("Change the input sequencing format to FASTA")
+                .long("inseqFasta")
         ).arg(
             Arg::new("outbed")
                 .about(
@@ -430,14 +607,24 @@ fn main() -> anyhow::Result<()> {
         )
         .get_matches();
 
-    let regions_file = matches.value_of("regions").unwrap();
-    let reference_file = matches.value_of("ref").unwrap();
+    let regions_file = matches.value_of("regions").unwrap_or("");
+    let reference_file = matches.value_of("ref").unwrap_or("");
     let input_file = matches.value_of("src").unwrap();
     let bed_file = matches.value_of("outbed");
     let seed = matches.value_of("seed");
     let kmer: usize = matches.value_of_t("kmer")?;
+    let inseq_fasta = matches.is_present("inputseqfasta");
 
-    if matches.is_present("fasta") {
+    if matches.is_present("inputseq") {
+        let input_seq_path = matches.value_of("inputseq").unwrap();
+        if matches.is_present("fasta") {
+            let input = Fastx::Fasta(input_file);
+            input.sim_input_seq(input_seq_path, seed, kmer, inseq_fasta)
+        } else {
+            let input = Fastx::Fastq(input_file);
+            input.sim_input_seq(input_seq_path, seed, kmer, inseq_fasta)
+        }
+    } else if matches.is_present("fasta") {
         let input = Fastx::Fasta(input_file);
         input.sim(reference_file, regions_file, bed_file, seed, kmer)
     } else {
